736c554fd9e42ec4612d8646df93cd26c7e720ae
Dushyant Kumar <dushyant.kumar@elitmus.com>
2019-10-04 00:03:58 +0530

    delete and edit files created and some changes on other files

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'articles/new'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:index, :show, :edit, :create, :new]
+  resources :articles, only: [:index, :show, :edit, :create, :new, :update, :destroy]
   root 'welcome#index'
 
 end
