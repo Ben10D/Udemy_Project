2c745c6cb8f537272a5c291bed8daa558dc8fd48
Dushyant Kumar <dushyant.kumar@elitmus.com>
2019-10-03 09:28:55 +0530

    initial level project change  on css html files

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'articles/new'
+  get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:index, :show, :edit, :create]
+  root 'welcome#index'
+end
