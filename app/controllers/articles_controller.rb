0c25694a8999aaaec8a31f4efeb6489d0aee2352
Dushyant Kumar <dushyant.kumar@elitmus.com>
2019-10-03 12:15:55 +0530

    many changes on new,show and index files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,20 @@
 class ArticlesController < ApplicationController
+  def index
+ @articles = Article.all
+  end
   def show
   	@article = Article.find(params[:id])
   end
   def new
   end
   def create
-  @article = Article.new(params.require(:article).permit(:title, :text))
+  @article = Article.new(article_params)
   @article.save
-  redirect_to @article
+  redirect_to articles_path
+  end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+    
   end
 end
