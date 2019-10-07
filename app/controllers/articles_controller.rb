090b7a43e3dfd9f4eb6e286bd8acc79a10e0bd57
Dushyant Kumar <dushyant.kumar@elitmus.com>
2019-10-07 20:26:32 +0530

    many changes and comment controller created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,44 +1,47 @@
 class ArticlesController < ApplicationController
-  def index
- @articles = Article.all
-  end
+   skip_before_action :verify_authenticity_token, :only => :create
+
   def show
   	@article = Article.find(params[:id])
+  	
+  end
+  def index
+  	@articles = Article.all
   end
   def new
-    @article = Article.new
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
+  @article = Article.new
+end
+ 
+def edit
+  @article = Article.find(params[:id])
+end
   def create
   @article = Article.new(article_params)
+ 
   if @article.save
-  redirect_to @article
-else 
-  render 'new' 
-end
+    redirect_to @article
+  else
+    render 'new'
   end
-  def update
-    @article = Article.find(params[:id])
-if @article.update(article_params)
-  redirect_to @article
+end
+ 
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
   else
     render 'edit'
-      
-    end    
   end
-  def destroy
+end
+ def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    redirect_to @article
-    
+ 
+    redirect_to articles_path
   end
-  private
+private
   def article_params
     params.require(:article).permit(:title, :text)
-    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'articles/new'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:index, :show, :edit, :create, :new, :update, :destroy]
+  resources :articles do
+  	resources :comments
+  end
   root 'welcome#index'
 
 end
